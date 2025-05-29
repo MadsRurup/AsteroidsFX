1783360ae98684d2ac7df588c6e4fb0bb09dbaa3
MadsRurup <143799277+MadsRurup@users.noreply.github.com>
2025-05-29 18:55:07 +0200

    Updated names (#4)

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -1,13 +1,13 @@
 import dk.sdu.cbse.common.bullet.BulletSPI;
-import dk.sdu.cbse.common.services.IProcessService;
+import dk.sdu.cbse.common.services.IProcessingService;
 import dk.sdu.cbse.player.PlayerPlugin;
 import dk.sdu.cbse.common.services.IPluginService;
-import dk.sdu.cbse.player.PlayerProcess;
+import dk.sdu.cbse.player.PlayerProcessing;
 
 module Player {
     requires Common;
     requires CommonBullet;
     uses BulletSPI;
     provides IPluginService with PlayerPlugin;
-    provides IProcessService with PlayerProcess;
+    provides IProcessingService with PlayerProcessing;
 }
